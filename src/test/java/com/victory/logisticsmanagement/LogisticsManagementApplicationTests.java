cfdbf0291337b53d9ad47b9be277b2e0bc0a5af5
Victor <i.victor@native.semicolon.africa>
2022-12-05 14:27:56 -0800

    first commit

---
+++ src/test/java/com/victory/logisticsmanagement/LogisticsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victory.logisticsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
